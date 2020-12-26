501de5a873fa231559620fc5be5b77da958597aa
eaaprf <60609318+eaaprf@users.noreply.github.com>
2020-12-26 23:06:11 +0100

    Add files via upload

---
+++ template_src/www/config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'artwalk',
+		wp_ws_url : 'https://prfrankild.dk/sem4/wp-appkit-api/artwalk',
+		wp_url : 'https://prfrankild.dk/sem4',
+		theme : 'wpak-theme-bootstrap-master',
+		version : '1',
+		app_type : 'cordova-template',
+		app_title : 'ArtWalk',
+		app_platform : 'android-cordova',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'on',
+		auth_key : 'nZsD3qf8){9Q]ixDmF%Dg3/mvmz P=*NBs1.({]/<B;/%1s;jviQV[|R~hU+lK.n',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
